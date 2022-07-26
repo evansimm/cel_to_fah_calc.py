c906b27a8089a166c79c5de19652c1cf6f5ebe3a
Evan Simmons <47922276+evansimm@users.noreply.github.com>
2022-07-26 13:01:54 -0400

    Create cel_to_fah_calc.py

---
+++ cel_to_fah_calc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#Celsius to Fahrenheit Calculator
+#This app will take the temperature as an input in Celsius and converts it to Fahrenheit. I made a program like this years ago in Java and wanted to do it again in python.
+
+Cel = input("Please enter temperature in Celsius: ")
+Fah = int(Cel) * 9/5 + 32
+Temp = str(Fah)
+print("The temperature is " + Temp + " degrees Fahrenheit.")
+quit()
